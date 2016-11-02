cfc12d40388294a0dc30b031458708d2e40fd5d5
Leo <liuznsn@leodemacbook-air.local>
2016-11-02 23:38:36 +0900

    make the ViewModel, model and network layer.

--- TrafficDemo/TrafficAPI.swift
+++ TrafficDemo/TrafficAPI.swift
@@ -7,3 +7,57 @@
 //
 
 import Foundation
+import Moya
+
+
+private extension String {
+    var URLEscapedString: String {
+        return self.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLHostAllowedCharacterSet())!
+    }
+}
+
+enum Traffic {
+    case FetchFlights()
+    case FetchTrains()
+    case FetchBuses()
+}
+
+extension Traffic: TargetType {
+    var baseURL: NSURL { return NSURL(string: "https://api.myjson.com")! }
+    var path: String {
+        switch self {
+        case .FetchFlights:
+            return "/bins/w60i"
+        case .FetchTrains:
+            return "/bins/3zmcy"
+        case .FetchBuses:
+            return "/bins/37yzm"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .GET
+    }
+    
+    var parameters: [String: AnyObject]? {
+        return nil
+    }
+    
+    var multipartBody: [MultipartFormData]? {
+        return nil
+    }
+
+    var parameterEncoding: ParameterEncoding {
+        switch self {
+        default:
+            return method == .GET ? .URL : .JSON
+        }
+    }
+    
+    var sampleData: NSData {
+        return "".dataUsingEncoding(NSUTF8StringEncoding)!
+    }
+    
+    
+    
+}
--- TrafficDemo/TrafficNetworkModel.swift
+++ TrafficDemo/TrafficNetworkModel.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+import Moya
+import Mapper
+import Moya_ModelMapper
+import RxOptional
+import RxSwift
+
+
+class TrafficNetworkModel {
+    
+    let provider: RxMoyaProvider<Traffic>
+    var flightElements = Variable<[Flights]>([])
+    let loading = Variable<Bool>(false)
+
+    private let disposeBag = DisposeBag()
+
+    init(provider:RxMoyaProvider<Traffic>) {
+        self.provider = provider
+     
+        let refreshRequest = loading.asObservable()
+            .flatMap { loading -> Observable<[Flights]> in
+                if loading {
+                    return Observable.empty()
+                } else {
+                    return self.fetchFlights().filterNil()
+                }
+        }
+      
+        let request = Observable
+            .of(refreshRequest)
+            .shareReplay(1)
+
+        Observable
+            .of(request.map { _ in false })
+            .merge()
+            .bindTo(loading)
+            .addDisposableTo(disposeBag)
+
+        request.subscribeNext { (flights) in
+            flights.subscribeNext({ (flights) in
+                self.flightElements = Variable.init(flights)
+            }).dispose()
+        }.addDisposableTo(disposeBag)
+        
+        
+    
+    }
+    
+    
+    func fetchFlights() -> Observable<[Flights]?> {
+       return self.provider
+        .request(Traffic.FetchFlights())
+        .debug()
+        .mapArrayOptional(Flights.self, keyPath: "items")
+    }
+}
